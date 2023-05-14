c67aec4ed3ca11d01955b0a37456bb9b2eb42e40
alonbarak-dev <alonbarak3110@gmail.com>
2023-05-14 16:12:38 +0300

    fixed excel

--- Custome_bot.py
+++ Custome_bot.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-data_file = "TBD"
+data_file = "Book1.xlsx"
 
 columns_user = ["ID", "Email", "University", "Role"]
 columns_events = ["User_ID", "Event_ID", "Description"]
 
 db = pd.read_excel(data_file, columns_user)
+print(db)
